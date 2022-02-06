8db47f923a8fceb7565cbaef3d24888be6fd631e
melezabi <egy.pianist@gmail.com>
2022-02-07 00:16:53 +0200

    Updated Yarn lockfile

---
+++ src/api/regestration-request/controllers/regestration-request.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  regestration-request controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::regestration-request.regestration-request');
---
+++ src/api/regestration-request/routes/regestration-request.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * regestration-request router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::regestration-request.regestration-request');
