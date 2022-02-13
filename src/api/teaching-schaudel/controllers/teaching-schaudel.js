388c2343c9bf6900ee1145f62bf2ede80e2d75eb
melezabi <egy.pianist@gmail.com>
2022-02-13 11:32:07 +0200

    Update database config

---
+++ src/api/teaching-schaudel/controllers/teaching-schaudel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  teaching-schaudel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::teaching-schaudel.teaching-schaudel');
---
+++ src/api/teaching-schaudel/routes/teaching-schaudel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * teaching-schaudel router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::teaching-schaudel.teaching-schaudel');
