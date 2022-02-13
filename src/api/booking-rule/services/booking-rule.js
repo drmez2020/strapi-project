94f3f765d8e36981651d584e1e25cbba4c1ef9e3
melezabi <egy.pianist@gmail.com>
2022-02-13 12:57:23 +0200

    Update database config

---
+++ src/api/booking-rule/services/booking-rule.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * booking-rule service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::booking-rule.booking-rule');
