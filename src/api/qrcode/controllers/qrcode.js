7e7552a3c56fe07d45f17341656e67953c1e5df5
melezabi <egy.pianist@gmail.com>
2022-02-16 14:06:02 +0200

    qr

--- src/api/qrcode/controllers/qrcode.js
+++ src/api/qrcode/controllers/qrcode.js
@@ -5,6 +5,7 @@ module.exports = {
   generate: async (ctx, next) => {
     try {
       const { data } = ctx.request.query;
+<<<<<<< HEAD
       const qr = QRCode(3, "H");
       qr.addData(data);
       qr.make();
@@ -15,6 +16,14 @@ module.exports = {
 =======
       ctx.body = `<img src="${qrImage}" style="height:800px" />`;
 >>>>>>> 82fcc7f4679b8d909c3e321178dc4732156cf674
+=======
+      console.log(data);
+      const qrImage = await QRCode.toDataURL(data);
+
+      ctx.body = {
+        qrImage,
+      };
+>>>>>>> parent of 387c3a1 (Qr update)
     } catch (err) {
       ctx.body = err;
     }
