da1794e2f416d0502dc5f7f37b7e2cf01c4ffbf7
melezabi <egy.pianist@gmail.com>
2022-02-16 02:05:42 +0200

    Updated Yarn lockfile

---
+++ src/api/booking-rule/controllers/booking-rule.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  booking-rule controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::booking-rule.booking-rule');
