6501f654926375fa2bb43423d3cc0898658440c8
melezabi <egy.pianist@gmail.com>
2022-02-16 19:03:03 +0200

    Updated Yarn lockfile

---
+++ src/api/teaching-schaudel/services/teaching-schaudel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * teaching-schaudel service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::teaching-schaudel.teaching-schaudel');
