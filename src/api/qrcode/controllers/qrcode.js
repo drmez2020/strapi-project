a95b9357fa712c1b4a65e3d4bcf1ac0f4b4ece88
melezabi <melezabi.freelancer@gmail.com>
2022-02-24 20:53:00 +0200

    year

--- src/api/qrcode/controllers/qrcode.js
+++ src/api/qrcode/controllers/qrcode.js
@@ -1,16 +1,15 @@
 "use strict";
-const QRCode = require("qrcode");
+const QRCode = require("qrcode-generator");
 
 module.exports = {
   generate: async (ctx, next) => {
     try {
       const { data } = ctx.request.query;
-      console.log(data);
-      const qrImage = await QRCode.toDataURL(data);
-
-      ctx.body = {
-        qrImage,
-      };
+      const qr = QRCode(3, "H");
+      qr.addData(data);
+      qr.make();
+      const qrImage = qr.createDataURL(8);
+      ctx.body = `<img src="${qrImage}" style="height:800px" />`;
     } catch (err) {
       ctx.body = err;
     }
