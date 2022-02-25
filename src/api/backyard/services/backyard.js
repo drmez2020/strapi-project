f97a36653dc332af63a2a166fef63c42f1e65ca6
melezabi <melezabi.freelancer@gmail.com>
2022-02-25 20:43:03 +0200

    Revert "Merge branch 'master' of https://github.com/drmez2020/strapi-project"
    This reverts commit 2a3a8fb3dcb58be7a32335a81c73a92e14aa042b, reversing
    changes made to 2b20be8e5ec8d29e0444e08886fae9914089078a.

--- src/api/backyard/services/backyard.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * backyard service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::backyard.backyard');
